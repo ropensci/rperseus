7348e9b4c57ee016b079fbe8782cdb60493bfee1
daranzolin <daranzolin@gmail.com>
2017-08-13 19:40:30 -0700

    updated catalog

--- R/data.R
+++ R/data.R
@@ -7,7 +7,6 @@
 #'   \item{lang}{language, "lat"(Latin)/"grc"(Greek)}
 #'   \item{groupname}{Could refer to author (e.g. "Aristotle") or corpus (e.g. "New Testament")}
 #'   \item{urn}{Uniform Resource Number}
-#'   \item{project}{Project identifier, truncated from urn}
 #'   \item{label}{Text title, e.g. "Phaedrus"}
 #'   \item{description}{text description}
 #'   ...
