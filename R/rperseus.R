43851b2d93394e8eedbe06a407c808b114f6ecf4
daranzolin <daranzolin@gmail.com>
2017-09-04 15:29:47 -0700

    added package-level documentation

---
+++ R/rperseus.R
@@ -0,0 +1,16 @@
+#' rperseus: A package to obtain texts from the Perseus Digital Library
+#'
+#' The package contains a catalog and a function. The catalog is \code{perseus_catalog} and can be
+#' perused to locate a particular text and its corresponding URN. Then pass the URN to \code{get_perseus_text}
+#' to obtain the text.
+#'
+#'
+#' @docType package
+#' @name rperseus
+#' @author David Ranzolin \email{daranzolin@@gmail.com}
+NULL
+#' Perseus Catalog
+#' @name perseus_catalog
+#' @docType data
+#' @keywords datasets
+NULL
