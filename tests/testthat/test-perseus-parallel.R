be56c099154b19a0546e82a33c61e72ed8d5439f
David Ranzolin <daranzolin@gmail.com>
2023-06-01 17:19:36 -0700

    more precise join/filter, fixes #14

--- tests/testthat/test-perseus-parallel.R
+++ tests/testthat/test-perseus-parallel.R
@@ -5,8 +5,7 @@ test_that("Can plot a perseus text parallel", {
 
   x <- tibble::tibble(label = c("Colossians", rep("1 Thessalonians", 2), "Romans"),
               excerpt = c("1.4", "1.3", "5.8", "8.35-8.39")) %>%
-    dplyr::left_join(perseus_catalog) %>%
-    dplyr::filter(language == "grc") %>%
+    dplyr::left_join(perseus_catalog %>% dplyr::filter(language == "grc")) %>%
     dplyr::select(urn, excerpt) %>%
     as.list() %>%
     purrr::pmap_df(get_perseus_text) %>%
